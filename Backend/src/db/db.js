081d3061238e05822f94fd83be4de24c4909e645
Aryan Aggarwal <aryan.agg72@gmail.com>
2026-08-07 05:31:30 +0530

    Changes in db

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -9,3 +9,33 @@ async function connectDb() {
 }
  
 module.exports = connectDb
+
+const neo4j = require("neo4j-driver")
+const dotenv = require("dotenv")
+
+dotenv.config();
+
+const driver = neo4j.driver(
+    process.env.COGNODB_URI,
+    neo4j.auth.basic(
+        process.env.COGNODB_USERNAME,
+        process.env.COGNODB_PASSWORD
+    )
+);
+
+const connectCognoDB = async () => {
+    try {
+        await driver.verifyConnectivity();
+        console.log("Connected to CognoDB Successfully...");
+    }
+    catch (error) {
+        console.error("CognoDB Connection Failed.");
+        console.error(error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = { 
+    driver,
+    connectCognoDB
+}
